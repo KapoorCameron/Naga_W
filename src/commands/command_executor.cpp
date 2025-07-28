9a076658cc5629fb8a44f437dac0d08d78321faa
ptsovathrty <cameron.kapoor@gmail.com>
2025-07-28 11:45:15 -0700

    Added beginning of command list given when user enters help command

--- src/commands/command_executor.cpp
+++ src/commands/command_executor.cpp
@@ -1,8 +1,27 @@
+#include<algorithm>
+#include<iomanip>
 #include<iostream>
+#include<string>
+#include<vector>
 
 #include"command_executor.h"
 
 void execute_help(const Command& cmd)
 {
-    std::cout << "help yourself\n";
+    std::vector<std::pair<std::string, std::string>> commands =
+    {
+        {"help", "Displays commands list."},
+        {"exit/quit", "Quits program"},
+        {"case", "Toggles case sensitivity"},
+    };
+
+    std::sort(commands.begin(), commands.end());
+    
+    std::cout << "\n//For command-specific details, enter \"help (command name)\"\\\\\n";
+
+    for (const auto& [command_name, description] : commands)
+    {
+        std::cout << std::left << std::setw(16) << command_name << description << "\n";
+    }
+    std::cout << "\n";
 }
